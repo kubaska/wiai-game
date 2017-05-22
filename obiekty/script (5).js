38eb9da1d4e021fbf6dd01fca9c17442c1869a0b
milena1231 <m.witkowska@zsek.olsztyn.pl>
2017-05-22 13:05:55 +0200

    obiekty
    4 obiekty (projekt)

---
+++ obiekty/script (5).js
@@ -0,0 +1,59 @@
+
+//koktajlMołotowa działa na przeciwnika negatywnie odejmując mu znaczną część życia i siły
+
+var koktajlMolotowa={
+	//przeciwnik
+	obrazenia=;
+	hp=;
+	//posiadacz
+	mana=;
+}
+
+// siećZasadzka działa na przeciwnika negatywnie ograniczając jego zręczność maksymalnie na czas 30 sekund w tym samym czasie przeciwnik nie ma sily, posiadacz sieci zdobywa inteligence 
+
+var siecZasadzka={
+	//przeciwnik
+	obrazenia=;
+	atak=;
+	refleks=;
+	hp=;
+	
+	//posiadacz
+	mana=;
+	atak=;
+
+	
+}
+
+//  działa na przeciwnika negatywnie odejmując większość siły i budowy, posiadacz zyskuje mana wytrzymalosc atak refleks
+
+var gunFire={
+	//przeciwnik
+	obrazenia=;
+	hp=;
+	wytrzymalosc=;
+	
+	//posiadacz
+	mana=;
+	wytrzymalosc=;
+	atak=;
+	refleks;
+	
+	
+}
+
+// kosaŻniwiarza bez wzgledu na obrone zmniejsza jego skille maksymalnie, postać wykożystująca kose żniwiarza traci mane o 50%
+
+var kosaZniwiarza= {
+	//przeciwnik
+	obrazenia=;
+	atak=;
+	czrostwo=;
+	wytzrymalosc=;
+	hp=;
+	
+	//posiadacz
+	mana=;
+	udzwig=;
+	
+}
