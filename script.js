db1d4e85067987419c9aed7cd14fc9d26e8028b2
syguu <kamciaa99@gmail.com>
2017-05-22 13:15:29 +0200

    obiekty_bronie
    bronie

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function update() {
-    map.set();
-    map.center();
-    
-    player.set();
-    player.getPos(player.css);
-    minimap.set();
-}
+var zwyklyMiecz={
+	udzwig: 3,
+	obrazenia: 2,
+	atak: 5, 
+	wytrzymaloscBroni: 30 //po ataku odejmuje x wytrzymałości broni / gdy wytrzymałość broni spadnie poniżej 0, to nie można jej używać
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
 
-window.onload = windowLoad => {
-    minimap.create();
-    update();
-}
+var zwyklaRozdzka= {
+	udzwig: 1,
+	obrazenia: 2,
+	atak: 2; 
+	wytrzymaloscBroni: 15
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
 
-window.onresize = windowSize => {
-    map.resize();
-    player.resize();
-    
-    update();
-}
+var zwyklyTopor={
+	udzwig: 3,
+	obrazenia: 2,
+	atak: 5, 
+	wytrzymaloscBroni: 25
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
 
-document.addEventListener('keyup', e => { player.move(e) });
+
+
