446212b5409d8dd97c86473e863e7d4b7add3685
Jakub Szlaga <jkbslg@gmail.com>
2017-05-22 17:02:35 +0200

    Zarys itemków
    Przywrócone.

---
+++ zarys/weapons.js
@@ -0,0 +1,25 @@
+var zwyklyMiecz={
+	udzwig: 3,
+	obrazenia: 2,
+	atak: 5, 
+	wytrzymaloscBroni: 30 //po ataku odejmuje x wytrzymałości broni / gdy wytrzymałość broni spadnie poniżej 0, to nie można jej używać
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
+
+var zwyklaRozdzka= {
+	udzwig: 1,
+	obrazenia: 2,
+	atak: 2; 
+	wytrzymaloscBroni: 15
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
+
+var zwyklyTopor={
+	udzwig: 3,
+	obrazenia: 2,
+	atak: 5, 
+	wytrzymaloscBroni: 25
+};
+//łatwo ją zdobyć, jest to powszechna broń na mapie
+
+
