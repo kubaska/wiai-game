7c074aa76d42bc21beeac9aa65ea2a7c5d817aec
Jakub Szlaga <jkbslg@gmail.com>
2017-05-22 16:48:08 +0200

    Revert "obiekty_bronie"
    This reverts commit db1d4e85067987419c9aed7cd14fc9d26e8028b2.

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-var zwyklyMiecz={
-	udzwig: 3,
-	obrazenia: 2,
-	atak: 5, 
-	wytrzymaloscBroni: 30 //po ataku odejmuje x wytrzymałości broni / gdy wytrzymałość broni spadnie poniżej 0, to nie można jej używać
-};
-//łatwo ją zdobyć, jest to powszechna broń na mapie
+function update() {
+    map.set();
+    map.center();
+    
+    player.set();
+    player.getPos(player.css);
+    minimap.set();
+}
 
-var zwyklaRozdzka= {
-	udzwig: 1,
-	obrazenia: 2,
-	atak: 2; 
-	wytrzymaloscBroni: 15
-};
-//łatwo ją zdobyć, jest to powszechna broń na mapie
+window.onload = windowLoad => {
+    minimap.create();
+    update();
+}
 
-var zwyklyTopor={
-	udzwig: 3,
-	obrazenia: 2,
-	atak: 5, 
-	wytrzymaloscBroni: 25
-};
-//łatwo ją zdobyć, jest to powszechna broń na mapie
+window.onresize = windowSize => {
+    map.resize();
+    player.resize();
+    
+    update();
+}
 
-
-
+document.addEventListener('keyup', e => { player.move(e) });
