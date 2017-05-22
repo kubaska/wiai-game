1fffd8df84822f5364673cdf5ed93111a99a3bef
Monika <monikasymko@gmail.com>
2017-05-22 13:09:13 +0200

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,46 @@
+var witaminkaAleksandry= {
+	//pozytywne działanie na gracza
+	czarostwo=+10;
+	obrazenia=+5;
+}
+var ognistaStrzala={
+	//negatywne działanie na przeciwnika i na gracza; leca kule ogniste(graficzny dodatek)- może
+	obrazenia=-15;
+	wytrzymalosc=-10;
+}
+
+var koktajlRemusa={
+	//działa pozytywnie na gracza, jest trudny do zalezienia, zabija przeciwnikow w obrebie x pól
+	
+	czarostwo=+10;
+	wytrzymalosc=+15;
+	obrazenia=+20;
+	hp=+15
+	
+}
+
+var eliksirMagdalenki ={
+	//działa pozytywnie na gracza
+	atak=+15;
+
+}
+
+var kask={
+	//gracz
+	wytrzymalosc=+10;
+	udzwig=-5;
+}
+
+var luk={
+	//gracz i moby
+	obrazenia=+15;
+	wytrzymalosc=+15;
+	udzwig=-10;
+	atak=+15;
+}
+
+var kusza={
+	//gracz i moby
+	obrazenia=+10;
+	udzwig=-5;
+}
