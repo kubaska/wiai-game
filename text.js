1eeead54805a244f8579ccb65cdfb86f967c81a1
kubaska <jkbslg@gmail.com>
2017-06-05 12:20:21 +0200

    Scrollable event text

---
+++ text.js
@@ -0,0 +1,24 @@
+var text = [];
+var textBuilder;
+var cssTextarea = document.querySelector(".textarea")
+
+function addText(message) {
+    if (text.length < 50) {
+        text.unshift(message);
+    }
+    else {
+        text.unshift(message);
+        text.pop();
+    }
+    textBuilder = "";
+    for (i = 0; i < text.length; i++){
+        textBuilder += text[i] + "<br/>";
+    }
+    cssTextarea.innerHTML = textBuilder;
+}
+
+function debugText() {
+    for (i = 0; i <= 20; i++){
+        addText("Hejka " + i);
+    }
+}
