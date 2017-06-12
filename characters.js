a1b55b907a65625bc470887b481f1df518375405
Jakub Szlaga <jkbslg@gmail.com>
2017-06-12 21:49:10 +0200

    New statistics

--- characters.js
+++ characters.js
@@ -1,47 +1,28 @@
-var baseArcher = {
-    strength: 0,
-	dexterity: 0,
-	build: 0,
-	intelligence: 0,
-    
-    hp: function () { return this.build + 30 },
-	mana: function() { return this.intelligence + 15 },
-	attack: function() { return this.strength + 15 },
-	dodge: function() { return this.dexterity + 35 }
-}
-
 var basePaladin = {
-    strength: 0,
-	dexterity: 0,
-	build: 0,
-	intelligence: 0,
-	
-	hp: function () { return this.build + 40 },
-	mana: function() { return this.intelligence + 30 },
-	attack: function() { return this.strength + 30 },
-	dodge: function() { return this.dexterity + 15 }
-}
-
-var baseNecromancer = {
-    strength: 0,
-	dexterity: 0,
-	build: 0,
-	intelligence: 0,
-    
-    hp: function () { return this.build + 25 },
-	mana: function() { return this.intelligence + 40 },
-	attack: function() { return this.strength + 10 },
-	dodge: function() { return this.dexterity + 30 }
+	hp: 200,
+	strength: 30,
+	skill: 5,
+    dodge: 0
 }
 
 var baseWarrior = {
-    strength: 0,
-	dexterity: 0,
-	build: 0,
-	intelligence: 0,
-    
-	hp: function () { return this.build + 50 },
-	mana: function() { return this.intelligence + 10 },
-	attack: function() { return this.strength + 40 },
-	dodge: function() { return this.dexterity + 5 }
+	hp: 150,
+	strength: 20,
+	skill: 5,
+    dodge: 0
 }
+
+var baseArcher = {
+	hp: 100,
+	strength: 25,
+	skill: 25,
+    dodge: 20
+}
+
+var baseNecromancer = {
+	hp: 100,
+	strength: 40,
+	skill: 30,
+    dodge: 10
+}
+
